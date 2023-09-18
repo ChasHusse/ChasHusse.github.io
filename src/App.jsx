08635136d26091ae7b40627f8f81d68e2a6b96ee
ChasHusse <115458547+ChasHusse@users.noreply.github.com>
2023-09-18 15:32:31 +0200

    Height changed in #full-page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ import ContactPage from './components/ContactPage'
 
 import { Route, Routes } from 'react-router-dom'
 
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 export default function App() {
   return (
     <div id="full-page">
