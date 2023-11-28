8d93e3c053b0a897ed1134bc8aad0552f1aa347c
ChasHusse <115458547+ChasHusse@users.noreply.github.com>
2023-11-28 11:46:53 +0100

    Portfolio changed

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -1,5 +1,6 @@
 import portImage1 from "../assets/port-1.png"
 import portImage2 from "../assets/port-2.png"
+import portImage3 from "../assets/port-3.png"
 
 
 
@@ -14,6 +15,14 @@ export default function PortfolioPage() {
                     <div className="port-info">
                         <h3>Personal website</h3>
                         <p>This is my first serious personal website that I have desgined and build by myself from scratch with React.</p>
+                    </div>
+                </div>
+                <div className="port-single">
+                    <img className="port-images" src={portImage3}></img>
+                    <div className="port-info">
+                        <h3>Fitness Website</h3>
+                        <p>A responsive website made with React and Typescript for a fitness gym.</p>
+                        <a href="https://chashusse.github.io/fitness_react_ts/" target="_blank">Press here to check it out!</a>
                     </div>
                 </div>
                 <div className="port-single">
